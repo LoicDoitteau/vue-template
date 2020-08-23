8c5de8d998a3e69f90fd607547c1830894b55945
LoicDoitteau <loic.doitteau@viacesi.fr>
2020-08-23 21:09:10 +0200

    Canvas setup

---
+++ src/components/RefixelCanvas.vue
@@ -0,0 +1,20 @@
+<template>
+    <canvas ref="canvas"/>
+</template>
+
+<script lang="ts">
+import { Component, Provide, Vue } from 'vue-property-decorator';
+
+@Component
+export default class RefixelCanvas extends Vue {
+
+    @Provide("context") private context: WebGLRenderingContext | null = null;
+
+    private mounted() {
+        const canvas = this.$refs.canvas as HTMLCanvasElement;
+        this.context = canvas.getContext('webgl');
+        this.context?.clearColor(0.1, 0.7, 0.3, 1);
+        this.context?.clear(this.context.COLOR_BUFFER_BIT);
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
+    <refixel-canvas/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import RefixelCanvas from '@/components/RefixelCanvas.vue';
 
+@Component({
+  components: {
+    RefixelCanvas
+  }
+})
 export default class Home extends Vue {
   
 }
